6c47f356dd31cf6752e917199f592b6ddd2a5516
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2024-11-25 08:09:31 -0600

    Agregado script start para Render5

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,17 +2,17 @@ const jwt = require('jsonwebtoken');
 
 // Generar un token
 function generateToken(userId) {
-    return jwt.sign({ userId }, 'tu_secreto_jwt', { expiresIn: '1h' });
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
 }
 
 // Middleware para autenticar el token
 function authenticateToken(req, res, next) {
     const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
-    if (!token) return res.sendStatus(401);
+    if (!token) return res.status(401).json({ message: 'Token no proporcionado' });
 
-    jwt.verify(token, 'tu_secreto_jwt', (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.userId = user.userId;
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Token no válido' });
+        req.userId = user.userId; // Asigna el `userId` al objeto `req` para que pueda ser accesible en las rutas protegidas
         next();
     });
 }
