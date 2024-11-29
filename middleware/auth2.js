075ac50f169d4a00b01ddd2109a254e84256dc91
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2024-11-28 21:58:35 -0600

    Agregado script start para Render

--- middleware/auth2.js
+++ middleware/auth2.js
@@ -3,28 +3,27 @@ const mongoose = require('mongoose');
 
 // Generar un token
 function generateToken(userId, role) {
-    return jwt.sign({ userId, role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ userId, role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 }
 
 // Middleware para autenticar el token
 function authenticateToken(req, res, next) {
-    const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
-    if (!token) return res.status(401).json({ message: 'Token no proporcionado' });
+  const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Token no proporcionado' });
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ message: 'Token no válido' });
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token no válido' });
 
-        // Convertir userId a ObjectId si es necesario
-        const userId = user.userId ? new mongoose.Types.ObjectId(user.userId) : null;
+    // Validamos que el userId sea un ObjectId válido si es necesario
+    // Si el userId es un string, no necesitas validarlo como ObjectId
+    if (user.userId && !mongoose.Types.ObjectId.isValid(user.userId)) {
+      return res.status(400).json({ message: 'El userId en el token no es un ObjectId válido' });
+    }
 
-        // Validar si es un ObjectId válido
-        if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
-            return res.status(400).json({ message: 'El userId en el token no es un ObjectId válido' });
-        }
-
-        req.user_id = { userId, role: user.role };
-        next();
-    });
+    // Almacena el userId y role en el request para usarlos en las rutas
+    req.user_id = { userId: user.userId, role: user.role };
+    next();
+  });
 }
 
 module.exports = { generateToken, authenticateToken };
--- routes/serviceRequests.js
+++ routes/serviceRequests.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Patient = require('../models/Patient');
-const { authenticateToken } = require('../middleware/auth');
+const { authenticateToken } = require('../middleware/auth2');
 
 const router = express.Router();
 
