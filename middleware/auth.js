2ebbd0fb9523b3a2aaea4b7af7aed303acf902b3
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2024-11-28 19:26:22 -0600

    Agregado script start para Render5

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,44 +1,25 @@
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
-// Generar un token JWT con `userId` y `role`
+// Generar un token
 function generateToken(userId, role) {
-  if (!mongoose.Types.ObjectId.isValid(userId)) {
-    throw new Error('El userId proporcionado no es un ObjectId válido');
-  }
-
-  return jwt.sign({ userId: userId.toString(), role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  return jwt.sign({ userId, role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 }
 
-// Middleware para autenticar y validar el token
+// Middleware para autenticar el token
 function authenticateToken(req, res, next) {
-  const authorizationHeader = req.headers['authorization'];
+  const token = req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
+  if (!token) return res.status(401).json({ message: 'Token no proporcionado' });
 
-  // Extraer el token del encabezado `Authorization`
-  if (!authorizationHeader) {
-    return res.status(401).json({ message: 'Token no proporcionado' });
-  }
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return res.status(403).json({ message: 'Token no válido' });
 
-  const token = authorizationHeader.split(' ')[1];
-  if (!token) {
-    return res.status(401).json({ message: 'Token no proporcionado' });
-  }
-
-  // Verificar el token y validar el contenido
-  jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
-    if (err) {
-      return res.status(403).json({ message: 'Token no válido', error: err.message });
-    }
-
-    const { userId, role } = payload;
-
-    // Validar que el userId sea un ObjectId válido
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
+    // Validamos que el userId sea un ObjectId válido
+    if (!mongoose.Types.ObjectId.isValid(user.userId)) {
       return res.status(400).json({ message: 'El userId en el token no es un ObjectId válido' });
     }
 
-    // Añadir datos del usuario autenticado al request
-    req.user_id = { userId: new mongoose.Types.ObjectId(userId), role };
+    req.user_id = { userId: new mongoose.Types.ObjectId(user.userId), role: user.role };
     next();
   });
 }
