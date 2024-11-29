3fe00e93fd4f0548c426d5c3cf73237f812701b0
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2024-11-28 22:49:15 -0600

    Agregado script start para Render

--- routes/serviceRequests.js
+++ routes/serviceRequests.js
@@ -10,6 +10,100 @@ const router = express.Router();
  *   description: Endpoints para la gestión de Servicios
  */
 
+// Ruta GET para obtener todas las solicitudes de servicio
+/**
+ * @swagger
+ * /service-requests:
+ *   get:
+ *     summary: Obtener todas las solicitudes de servicio
+ *     tags:
+ *       - ServiceRequests
+ *     responses:
+ *       200:
+ *         description: Lista de solicitudes de servicio
+ *       500:
+ *         description: Error al obtener las solicitudes de servicio
+ */
+router.get('/', authenticateToken, async (req, res) => {
+    try {
+        const serviceRequests = await ServiceRequest.find(); // Obtiene todas las solicitudes de servicio
+        res.json(serviceRequests); // Devuelve las solicitudes encontradas
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Error al obtener las solicitudes de servicio' });
+    }
+});
+
+// Ruta POST para crear una solicitud de servicio
+/**
+ * @swagger
+ * /service-requests:
+ *   post:
+ *     summary: Crear una nueva solicitud de servicio
+ *     tags:
+ *       - ServiceRequests
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               user_id:
+ *                 type: string
+ *               nurse_id:
+ *                 type: string
+ *               patient_ids:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *               estado:
+ *                 type: string
+ *               detalles:
+ *                 type: string
+ *               fecha:
+ *                 type: string
+ *                 format: date-time
+ *               tarifa:
+ *                 type: integer
+ *               pago_realizado:
+ *                 type: boolean
+ *               pago_liberado:
+ *                 type: boolean
+ *     responses:
+ *       201:
+ *         description: Solicitud de servicio creada
+ *       500:
+ *         description: Error al crear la solicitud de servicio
+ */
+router.post('/', authenticateToken, async (req, res) => {
+  const { user_id, nurse_id, patient_ids, estado, detalles, fecha, tarifa, pago_realizado, pago_liberado } = req.body;
+  if (!user_id) {
+    return res.status(400).json({ message: 'El campo user_id es obligatorio' });
+  }
+
+  try {
+    const newServiceRequest = new ServiceRequest({
+      user_id,
+      nurse_id,
+      patient_ids,
+      estado,
+      detalles,
+      fecha,
+      tarifa,
+      pago_realizado,
+      pago_liberado,
+    });
+
+    await newServiceRequest.save(); // Guardar la nueva solicitud
+
+    const { _id, ...serviceData } = newServiceRequest.toObject(); // Excluir `_id` de la respuesta
+    res.status(201).json(serviceData);
+  } catch (error) {
+    res.status(400).json({ message: 'Error al crear el Servicio', error: error.message });
+  }
+});
+
 // Ruta GET para obtener las solicitudes de servicio de un enfermero específico (filtrando por nurse_id)
 /**
  * @swagger
