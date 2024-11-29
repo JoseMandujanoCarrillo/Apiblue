5d9a198074bb9a2929221146b2e379b2d05036ae
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2024-11-28 19:16:49 -0600

    Agregado script start para Render5

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,20 +7,29 @@ function authenticateToken(req, res, next) {
     return res.status(401).json({ message: 'Token no proporcionado' });
   }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
     if (err) {
       return res.status(403).json({ message: 'Token no válido', error: err.message });
     }
 
-    // Validar el userId dentro del token
-    const userId = user.userId.userId || user.userId; // Soporte para ambos formatos
+    // Detectar la estructura del token (cadena directa o anidada)
+    const userId = typeof payload.userId === 'string' 
+      ? payload.userId 
+      : payload.userId?.userId;
+
+    const role = typeof payload.userId === 'string'
+      ? payload.role
+      : payload.userId?.role;
+
+    // Validar el userId como ObjectId
     if (!mongoose.Types.ObjectId.isValid(userId)) {
       return res.status(400).json({ message: 'El userId en el token no es un ObjectId válido' });
     }
 
+    // Pasar el userId y el role al request
     req.user_id = {
       userId: new mongoose.Types.ObjectId(userId),
-      role: user.userId.role || user.role
+      role
     };
 
     next();
