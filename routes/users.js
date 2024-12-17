c6017dfa2cb61ecdb9922ba07587ee682de3b738
JoseMandujanoCarrillo <mandujanocarrillo27@gmail.com>
2024-12-17 11:53:14 -0600

    Agregado script start para Render

--- routes/users.js
+++ routes/users.js
@@ -23,10 +23,6 @@ const router = express.Router();
  *         application/json:
  *           schema:
  *             type: object
- *             required:
- *               - name
- *               - user_name
- *               - password
  *             properties:
  *               name:
  *                 type: string
@@ -36,10 +32,8 @@ const router = express.Router();
  *                 type: string
  *               foto:
  *                 type: string
- *               comidaFavorita:
+ *               verificado:
  *                 type: string
- *               descuentoNavideño:
- *                 type: number
  *     responses:
  *       201:
  *         description: Usuario registrado exitosamente
@@ -50,7 +44,7 @@ router.post('/register', async (req, res) => {
   try {
     const newUser = new User(req.body);
     await newUser.save();
-    const token = generateToken({ userId: newUser._id, role: 'usuario' });
+    const token = generateToken(newUser._id);
     res.status(201).json({ user: newUser, token });
   } catch (error) {
     res.status(400).json({ message: 'Error al registrar el usuario', error: error.message });
@@ -69,9 +63,6 @@ router.post('/register', async (req, res) => {
  *         application/json:
  *           schema:
  *             type: object
- *             required:
- *               - user_name
- *               - password
  *             properties:
  *               user_name:
  *                 type: string
@@ -98,6 +89,74 @@ router.post('/login', async (req, res) => {
     res.json({ token, role: 'usuario' });
   } catch (error) {
     res.status(500).json({ message: 'Error en el inicio de sesión', error: error.message });
+  }
+});
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Obtener una lista de usuarios con paginación
+ *     tags: [Users]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *         description: Número de la página (por defecto 1)
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           default: 10
+ *         description: Número de elementos por página (por defecto 10)
+ *     responses:
+ *       200:
+ *         description: Lista de usuarios con paginación
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 total:
+ *                   type: integer
+ *                   description: Número total de usuarios
+ *                 page:
+ *                   type: integer
+ *                   description: Página actual
+ *                 limit:
+ *                   type: integer
+ *                   description: Límite de elementos por página
+ *                 users:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Error del servidor
+ */
+router.get('/', authenticateToken, async (req, res) => {
+  try {
+    const { page = 1, limit = 10 } = req.query;
+
+    const skip = (page - 1) * limit;
+
+    const users = await User.find()
+      .skip(skip)
+      .limit(Number(limit));
+
+    const total = await User.countDocuments();
+
+    res.status(200).json({
+      total,
+      page: Number(page),
+      limit: Number(limit),
+      users
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error en el servidor', error: error.message });
   }
 });
 
@@ -133,40 +192,6 @@ router.get('/panel', authenticateToken, (req, res) => {
     message: "Bienvenido al panel principal",
     opciones: ["Buscar Enfermeros", "Mis Pacientes"]
   });
-});
-
-/**
- * @swagger
- * /users/me:
- *   get:
- *     summary: Obtener la información del usuario autenticado
- *     tags: [Users]
- *     security:
- *       - bearerAuth: []
- *     responses:
- *       200:
- *         description: Información del usuario autenticado
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 user:
- *                   $ref: '#/components/schemas/User'
- *       401:
- *         description: No autorizado, token no proporcionado o inválido
- */
-router.get('/me', authenticateToken, async (req, res) => {
-  try {
-    // `req.user` contiene el userId después de ser decodificado por `authenticateToken`
-    const user = await User.findById(req.user.userId);
-    if (!user) {
-      return res.status(404).json({ message: 'Usuario no encontrado' });
-    }
-    res.status(200).json({ user });
-  } catch (error) {
-    res.status(500).json({ message: 'Error al obtener el usuario', error: error.message });
-  }
 });
 
 module.exports = router;
